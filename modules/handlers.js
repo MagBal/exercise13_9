7de6bdca6f3f85f0a9e960e5eb74e52d9a1924ab
Magda Balun <magi@op.pl>
2018-01-05 23:52:05 +0100

    Add index.js, package.json, handlers.js and server.js files

---
+++ modules/handlers.js
@@ -0,0 +1,49 @@
+var formidable = require('formidable');
+var fs = require('fs');
+
+var fileName;
+
+exports.upload = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania upload.");
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files) {
+        response.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        fileName = fields.title;
+        if (fileName == null || fileName.length == 0) {
+            fileName = files.upload.name;
+        }
+        response.write("received image: " + fileName + "<br/>");
+        fs.renameSync(files.upload.path, fileName);
+        response.write("<img src='/show' />");
+        response.end();
+    });
+}
+
+exports.welcome = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania welcome.");
+    fs.readFile('templates/start.html', function(err, html) {
+        response.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8"
+        });
+        response.write(html);
+        response.end();
+    });
+}
+
+exports.error = function(request, response) {
+    console.log("Nie wiem co robić.");
+    response.write("404 :(");
+    response.end();
+}
+
+exports.show = function(request, response) {
+    fs.readFile(fileName, "binary", function(error, file) {
+        response.writeHead(200, {
+            "Content-Type": "image/jpg"
+        });
+        response.write(file, "binary");
+        response.end();
+    });
+}
